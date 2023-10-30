afe536551afc629ff3fc6829544a604c61242d53
THE-FLIER <2397252522@qq.com>
2023-10-30 14:24:25 +0800

    attention update

--- labelmetocooc.py
+++ labelmetocooc.py
@@ -1,5 +1,6 @@
 #lambelme  标 转  yolov
 import json
+import os
 '''
 会在同一目录下生成txt训练文件
 '''
@@ -19,17 +20,16 @@ def lambelme_json_label_to_yolov_seg_label(json_path):
         height=1440
         width=2560 
         np_w_h = np.array([[width, height]], np.int32)
-        txt_file = json_file.replace(".json", ".txt")
+        txt_file = 'datasets/bookshelf - 副本/labels/train/'+os.path.basename(json_file.replace(".json", ".txt"))
         f = open(txt_file, "a")
         for point_json in json_info["shapes"]:
-
             txt_content = ""
             np_points = np.array(point_json["points"], np.int32)
             norm_points = np_points / np_w_h
             norm_points_list = norm_points.tolist()
-            txt_content += "0 " + " ".join([" ".join([str(cell[0]), str(cell[1])]) for cell in norm_points_list]) + "\n"
+            txt_content += "0" + " ".join([" ".join([str(cell[0]), str(cell[1])]) for cell in norm_points_list]) + "\n"
             f.write(txt_content)
 
 if __name__=="__main__":
-    json_path = "test_pics/test2/5_1/"
+    json_path = "datasets/bookshelf/labels/test/"
     lambelme_json_label_to_yolov_seg_label(json_path)
