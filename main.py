7bbeba3a276218e0d6f7651a158f00a61bac9bad
THE-FLIER <2397252522@qq.com>
2023-11-06 14:52:57 +0800

    update

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 import sys
 # sys.path.append('D:/yolo_new/ultralytics')
 if __name__ == '__main__':
-    model = YOLO("ultralytics/cfg/models/v8/yolov8-pose-p6-cbam.yaml").load('./models/yolov8x-pose-p6.pt')
-    model.train(**{'cfg': 'ultralytics/cfg/default-point.yaml'})
+    model = YOLO("ultralytics/cfg/models/v8/yolov8m-seg-cbam.yaml").load('./models/yolov8m-seg.pt')
+    model.train(**{'cfg': 'ultralytics/cfg/default.yaml'})
 
